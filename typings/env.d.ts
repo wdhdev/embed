2dce72a635992ee26fae57f7bd0168de832dab49
William Harrison <william@williamdavidharrison.com.au>
2024-02-28 18:26:38 +0800

    custom port + git pull

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -3,6 +3,7 @@ import { Snowflake } from "discord.js";
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
+            port: number;
             sentry_dsn: string;
         }
     }
