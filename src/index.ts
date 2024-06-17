ba36e20316a195cf182188b8acbe76aab9e604f2
William Harrison <william@williamdavidharrison.com.au>
2024-06-17 10:49:28 +0800

    update sentry + other deps

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,22 @@ require("dotenv").config();
 const port = process.env.port || 80;
 
 import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import bodyParser from "body-parser";
 import cors from "cors";
 
 Sentry.init({
     dsn: process.env.sentry_dsn,
     integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-        new Sentry.Integrations.Express({ app }),
-        ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations()
+        nodeProfilingIntegration()
     ],
-    tracesSampleRate: 1.0
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0
 })
 
 import router from "./util/router";
 
-app.use(Sentry.Handlers.requestHandler());
-app.use(Sentry.Handlers.tracingHandler());
-
-app.use(cors({ origin: "*" }));
+app.use(cors<express.Request>({ origin: "*" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -32,21 +29,8 @@ app.set("view engine", "ejs");
 
 app.use("/", router);
 
-app.use(Sentry.Handlers.errorHandler());
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(port, () => {
     console.log(`Listening on Port: ${port}`);
 })
-
-import { exec } from "child_process";
-
-// Automatic Git Pull
-setInterval(() => {
-    exec("git pull", (err: any, stdout: any) => {
-        if(err) return console.log(err);
-        if(stdout.includes("Already up to date.")) return;
-
-        console.log(stdout);
-        process.exit();
-    })
-}, 30 * 1000) // 30 seconds
