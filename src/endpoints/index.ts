1edb2bf9ddb56facf3cd8d8b9139a67994fc8e4c
William Harrison <87287585+wdhdev@users.noreply.github.com>
2024-12-03 08:13:21 +0800

    static

--- src/endpoints/index.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export default async (req: Request, res: Response) => {
-    res.render("index");
-}
--- src/util/routes.ts
+++
@@ -1,7 +0,0 @@
-import embed from "../endpoints/embed";
-import index from "../endpoints/index";
-
-export default {
-    embed: embed,
-    index: index
-}
